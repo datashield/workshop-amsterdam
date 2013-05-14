5cf32b2e4a138d7f3150ec79a58e83d4045d96e3
Yannick Marcon <yannick.marcon@obiba.org>
2013-05-14 10:54:05 -0400

    Kora added.

--- tests/opals.R
+++ tests/opals.R
@@ -14,7 +14,8 @@ ncds <- opal.login(url='https://lamp-api-32.rcs.le.ac.uk:8443', opts=credentials
 hunt <- opal.login(url='https://opal.medisin.ntnu.no', opts=credentials)
 prevend <- opal.login(url='https://molgenis34.target.rug.nl:8443', opts=credentials)
 finrisk <- opal.login(url='https://opal.thl.fi:8443', opts=credentials)
-opals<-list(ncds, hunt, prevend, finrisk)
+kora <- opal.login(url='https://146.107.6.14:8443/', opts=credentials)
+opals<-list(ncds, finrisk, hunt, prevend, kora)
 
 # NCDS
 opal.table(ncds,'hop','HOP')
@@ -36,5 +37,13 @@ opal.table(prevend,'hop-prevend','HOP')
 datashield.assign(prevend,'HOS','hop-prevend.HOP:HLTH_OBESE_STRICT')
 datashield.aggregate(prevend,'length(HOS)')
 
+# KORA
+opal.table(kora,'bioshare','HOP')
+datashield.assign(kora,'HOS','bioshare.HOP:HLTH_OBESE_STRICT')
+datashield.aggregate(kora,'length(HOS)')
+
 # Summary of HOS for all studies
 datashield.aggregate(opals,'summary(HOS)')
+
+# Total number of participants
+Reduce(sum,datashield.aggregate(opals,'length(HOS)'))
