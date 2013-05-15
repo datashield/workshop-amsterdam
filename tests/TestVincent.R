7d7b27979fb52d044b61fd2f82f091c689ce7f26
Vincent Ferretti <vincent.ferretti@oicr.on.ca>
2013-05-15 16:57:41 -0400

    a test for the workshop on real data

---
+++ tests/TestVincent.R
@@ -0,0 +1,85 @@
+#!/usr/bin/Rscript
+
+library('datashieldclient')
+
+GlobalAssign <- function(opals, variable) { 
+  lapply(opals, function(o) {
+   datashield.assign(o, variable, paste(o$table, variable, sep=":"))
+  })
+}
+
+# Login in opal of each study
+credentials <- list(
+  sslcert=paste0(Sys.glob('~'),'/.ssh/publickey.pem'),
+  sslkey=paste0(Sys.glob('~'),'/.ssh/privatekey.pem'),
+  ssl.verifyhost=0,
+  ssl.verifypeer=0,
+  sslversion=3)
+
+ncds <- opal.login(url='https://lamp-api-32.rcs.le.ac.uk:8443', opts=credentials)
+hunt <- opal.login(url='https://opal.medisin.ntnu.no', opts=credentials)
+prevend <- opal.login(url='https://molgenis34.target.rug.nl:8443', opts=credentials)
+finrisk <- opal.login(url='https://opal.thl.fi:8443', opts=credentials)
+kora <- opal.login(url='https://146.107.6.14:8443/', opts=credentials)
+#opals<-list(ncds, finrisk, hunt, prevend, kora)
+opals<-list(ncds, finrisk, hunt, kora)
+
+ncds$table <- "hop.HOP"
+hunt$table <- "bioshare.HOP"
+prevend$table <- "hop-prevend.HOP"
+finrisk$table <- "HOPcopy.HOP"
+kora$table <- "bioshare.HOP"
+
+
+# Q1: ARE THERE DIFFERENCES IN CHOLESTEROL LEVELS BETWEEN MALES AND FEMALES?
+# using the function 'datashield.t.test'
+# datashield.t.test(opals, quote(D$LAB_TSC), quote(D$GENDER))
+# using the function 'datashield.glm'
+# GLM test
+
+GlobalAssign(opals, "LAB_TSC")
+GlobalAssign(opals, "GENDER")
+a1 <- datashield.glm(opals, LAB_TSC ~ GENDER, quote(gaussian), quote(20))
+summary(a1)
+
+
+# Q2: DO PEOPLE WHO ARE OBESE HAVE HIGHER GLUCOSE LEVELS? (GLUCOSE LEVEL BY BMI CATEGORY)
+
+GlobalAssign(opals, "LAB_GLUC_NONFASTING")
+GlobalAssign(opals, "PM_BMI_CATEGORIAL")
+a2 <- datashield.glm(opals, LAB_GLUC_NONFASTING ~ PM_BMI_CATEGORIAL, quote(gaussian), quote(20))
+summary(a2)
+
+
+# Q3: IS THE PREVALENCE OF DIABETES HIGHER AMONG THE OBESE PEOPLE?
+# using the function 'datashield.table.2d' 
+GlobalAssign(opals, "DIS_DIAB")
+datashield.table.2d(opals, quote(DIS_DIAB), quote(PM_BMI_CATEGORIAL))
+
+# using the function 'datashield.glm' D$MEDI_LPD
+a3 <- datashield.glm(opals, DIS_DIAB ~ PM_BMI_CATEGORIAL, quote(binomial), quote(20))
+summary(a3)
+
+
+# Q4: ARE THE VARIABLES TOTAL SERUM CHOLESTEROL AND TOTAL HDL CHOLESTEROL ASSOCIATED
+# visualize using the function 'datashield.heatmap.plot'
+GlobalAssign(opals, "LAB_HDL")
+datashield.heatmap.plot(opals, quote(LAB_TSC), quote(LAB_HDL))
+# using the function 'datashield.glm'
+a4 <- datashield.glm(opals, LAB_TSC ~ LAB_HDL, quote(gaussian), quote(20))
+summary(a4)
+
+
+# Q5: CAN WE PREDICT DIABETES, IN THE HOP 'POPULATION', USING SOME HOP VARIABLES?
+# predict using non-fasting glucose and bmi
+GlobalAssign(opals, "PM_BMI_CONTINUOUS")
+
+a5 <- datashield.glm(opals, DIS_DIAB ~ LAB_GLUC_NONFASTING + PM_BMI_CONTINUOUS, quote(binomial), quote(20))
+summary(a5)
+
+
+# Q6: ARE SOME HOP VARIABLES ASSOCIATED WITH A TAKING LIPID REDUCING MEDICATIONS?
+GlobalAssign(opals, "MEDI_LPD")
+
+a6 <- datashield.glm(opals, MEDI_LPD ~ LAB_TSC + PM_BMI_CONTINUOUS, quote(binomial), quote(20))
+summary(a6)
